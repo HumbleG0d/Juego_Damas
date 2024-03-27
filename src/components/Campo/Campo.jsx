ba587bd15d5a5dd3d8e8cc3571bc85e0bfa05ec9
HumbleG0d <humbleg0d7642@gmail.com>
2024-03-27 16:08:58 -0500

    Remove inputId

--- src/components/Campo/Campo.jsx
+++ src/components/Campo/Campo.jsx
@@ -1,11 +1,12 @@
 export default function Campo({ classNameLabel, classNameInput, htmlFor, name, type , register}) {
     
-    const inputId = `${htmlFor}-input`
-
+    const boolean = type === 'checkbox' ? false : true
     return (
         <>
-            <label className = {classNameLabel} htmlFor = {inputId} >{name}</label>
-            <input className={classNameInput} type={type} id={inputId} {...register(inputId)} />
+            <label className = {classNameLabel} htmlFor = {htmlFor} >{name}</label>
+            <input className={classNameInput} type={type} id={htmlFor} {...register(htmlFor, {
+                required: {value: boolean, message: `El campo ${name} es obligatorio`}
+            })} />
         </>
     
     )
