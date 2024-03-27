966158c101d228dbb7cb1063f6976a49778ea57c
HumbleG0d <humbleg0d7642@gmail.com>
2024-03-27 15:25:19 -0500

    Add id and change value htmlFor

--- src/components/Campo/Campo.jsx
+++ src/components/Campo/Campo.jsx
@@ -1,8 +1,11 @@
-export default function Campo({classNameLabel ,classNameInput , htmlFor , name , type}) {
+export default function Campo({ classNameLabel, classNameInput, htmlFor, name, type}) {
+    
+    const inputId = `${htmlFor}-input`
+
     return (
         <>
-            <label className = {classNameLabel} htmlFor = {htmlFor}>{name}</label>
-            <input className = {classNameInput} type = {type} />
+            <label className = {classNameLabel} htmlFor = {inputId} >{name}</label>
+            <input className = {classNameInput} type = {type} id = {inputId}/>
         </>
     
     )
