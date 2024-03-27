400182a1641e106d21c1763d861a793296de4004
HumbleG0d <humbleg0d7642@gmail.com>
2024-03-27 16:10:37 -0500

    Add method formState and Add errors

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -8,7 +8,7 @@ import {useForm} from 'react-hook-form'
 
 export default function FormLogin() {
 
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit , formState:{errors} } = useForm()
   
   const onSubmit = handleSubmit((data) => {
     console.log(data)
@@ -18,15 +18,21 @@ export default function FormLogin() {
         <>
           <Form onSubmit = {onSubmit}>
             <div className="div_campo">
-            <Campo classNameInput = "form_input" classNameLabel = "form_label-id" htmlFor="id" name="NOMBRE DE CUENTA" type="text" register={register}/>  
+            <Campo classNameInput="form_input" classNameLabel="form_label-id" htmlFor="id" name="NOMBRE DE CUENTA" type="text" register={register} />
+            {errors.id && <span className="form_error">Nombre de usuario no ingresado</span>}
             </div>
+            
             <div className="div_campo">
-            <Campo classNameInput="form_input" classNameLabel="form_label" htmlFor="password" name="CONTRASEÑA" type="password" register={register}/>            
+              <Campo classNameInput="form_input" classNameLabel="form_label" htmlFor="password" name="CONTRASEÑA" type="password" register={register}/>            
             </div>            
+            {errors.password && <span className="form_error">Contraseña no ingresada</span>}
+            
             <div className="div_campo-2">
-            <Campo classNameInput="form_check" classNameLabel="form_check" htmlFor="recordar" name="Recordarme" type="checkbox" register={register}/>    
+              <Campo classNameInput="form_check" classNameLabel="form_check" htmlFor="recordar" name="Recordarme" type="checkbox" register={register}/>    
             </div>
-            <Button classNameButon="buttonSesion" >Iniciar sesión</Button>
+            
+            <Button classNameButon="buttonSesion">Iniciar sesión</Button>
+              
             <a href="" className="form_enlace">Crear una cuenta gratuita</a>
           </Form>
         </>
