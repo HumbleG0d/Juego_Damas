6f77e923e037a9f2c95150c4db9362d39f6ee0e1
HumbleG0d <humbleg0d7642@gmail.com>
2024-03-27 15:30:33 -0500

    Add method register

--- src/components/Campo/Campo.jsx
+++ src/components/Campo/Campo.jsx
@@ -1,11 +1,11 @@
-export default function Campo({ classNameLabel, classNameInput, htmlFor, name, type}) {
+export default function Campo({ classNameLabel, classNameInput, htmlFor, name, type , register}) {
     
     const inputId = `${htmlFor}-input`
 
     return (
         <>
             <label className = {classNameLabel} htmlFor = {inputId} >{name}</label>
-            <input className = {classNameInput} type = {type} id = {inputId}/>
+            <input className={classNameInput} type={type} id={inputId} {...register(inputId)} />
         </>
     
     )
