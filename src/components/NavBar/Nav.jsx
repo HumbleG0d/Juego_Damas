aeab114cf5e3352fae224ac69af34cd60b7bc77b
HumbleG0d <humbleg0d7642@gmail.com>
2024-03-29 22:11:32 -0500

    Delete coments

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,9 +1,4 @@
-//import { HiMiniBars4 } from "react-icons/hi2";
-//import { navigationItems } from './navigationItems'
-//import { useState } from 'react'
 export default function Nav() { 
-  //  const [openMenu, setOpenMenu] = useState(false)
-    
     return (
         <header className="header">
             <div className="div_header">
@@ -18,12 +13,6 @@ export default function Nav() {
                     <li><a href="">Info</a></li>
                     <li><a href="">Register</a></li>
             </ul>
-
-{/*<div className='navbar_menu-container'>
-                <HiMiniBars4 className='navbar_menu' onClick={() => setOpenMenu(!openMenu)} />
-            </div> */}
-                
-
         </nav>
 
        </header>
