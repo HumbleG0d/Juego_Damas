34bd81ac98be6ac33c7e29eadb74676b60a8de95
HumbleG0d <humbleg0d7642@gmail.com>
2024-03-31 12:01:33 -0500

    Change not

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Header from './components/Header'
 export default function App() {
     return (
       <>
-         <Header />
-          <Main />   
+        <Header />
+        <Main />
       </>
     )
 }
