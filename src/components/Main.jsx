6a312f68573942d912b2113584fa8d0307602a7c
HumbleG0d <humbleg0d7642@gmail.com>
2024-03-31 11:59:10 -0500

    Add logic to display the form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,32 @@
 import '../styles/main.css'
 import Button from './Button/Button'
 import Checkerboard from './ModelViewer/Checkerboard'
+import FormLogin from './FormLogin'
+import { useState} from 'react'
 export default function Main() { 
+    const [showLoginForm, setShowLoginForm] = useState(false)
+
+    const [opacity, setOpacity] = useState(false)
+
+    const handleBecomeMasterClick = () => { 
+        setShowLoginForm(true)
+        setOpacity(true)
+    }
+
+    if (opacity) {
+        document.body.style.backgroundColor = '#404040'
+        document.body.style.backdropFilter = 'blur(15px)'
+    }
+
     return (
         <section className='section_main'>
             <div className='div_section'>
                 <div className='div_section-info'>
                     <h1>TAKE YOUR CHECKERS GAME TO THE NEXT LEVEL</h1>
                     <p>Connet with top ranked chess players from around the world and learn the secrets to becoming a checkers master.</p>
-                    <Button classNameButon='section_button'>Become A Master</Button>
+                    <div className='div_button'>
+                    <Button classNameButon='section_button' onClick ={handleBecomeMasterClick} >Become A Master</Button>
+                    </div>
                 </div>
 
                 <div className='div_section-img'>
@@ -17,6 +35,7 @@ export default function Main() {
                     </div>
                 </div>
             </div>
+            {showLoginForm && <FormLogin />}
         </section>
     )
 }
