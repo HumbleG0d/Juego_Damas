a68532f136171a231f94626c71301ae7715ce641
HumbleG0d <humbleg0d7642@gmail.com>
2024-03-31 18:28:10 -0500

    Add formating code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import './App.css'
 import './styles/normalize.css'
 import Main from './components/Main'
 import Header from './components/Header'
-export default function App() {
-    return (
+export default function App () {
+  return (
       <>
-        <Header />
-        <Main />
-      </>
-    )
+      <Header />
+      <Main/>
+    </>
+  )
 }
