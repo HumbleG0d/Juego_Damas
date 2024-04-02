d9826f51ec4ae0cfd8022e8a1176527a17477398
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-01 21:59:46 -0500

    Add Routes and Route for the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
 import './styles/normalize.css'
+import './styles/nav.css'
 import Main from './components/Main'
-import Header from './components/Header'
+import Instructions from './components/Instructions'
+import Ranking from './components/Ranking'
+import { Routes, Route } from 'react-router-dom'
+import Nav from './components/NavBar/Nav'
+
 export default function App () {
   return (
-      <>
-      <Header />
-      <Main/>
+    <>
+       <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/instructions" element={<Instructions />} />
+        <Route path="/ranking" element={<Ranking />} />
+      </Routes>
     </>
   )
 }
