57a81d3e09011144183776d23bcfbe800694ff31
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-02 10:10:32 -0500

    Add logic for password requirements

--- src/components/Campo/Campo.jsx
+++ src/components/Campo/Campo.jsx
@@ -5,9 +5,17 @@ export default function Campo({ classNameLabel, classNameInput, htmlFor, name, t
         <>
             <label className = {classNameLabel} htmlFor = {htmlFor} >{name}</label>
             <input className={classNameInput} type={type} id={htmlFor} {...register(htmlFor, {
-                required: {value: boolean, message: `${name.toLowerCase()} es requerido` , ...(type === 'email' && {pattern: {value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, message: 'correo invalido'}})}
+                required: {
+                    value: boolean, message: `${name.toLowerCase()} es requerido`, ...(type === 'email' && { pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, message: 'correo invalido' } }), 
+                    ...(type === 'password' && {
+                        minLength: { value: 8, message: 'La contraseña debe tener al menos 8 caracteres' },
+                        pattern: {
+                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/,
+                            message: 'La contraseña debe contener al menos una letra mayúscula, un número, y un carácter especial'
+                        }
+                    })
+                }
             })} />
         </>
-    /*TODO: agregar requerimientos para que la contraseña sea de longitud 8 que contenga letra mayuscula , numeros , caracteres */
     )
 }
