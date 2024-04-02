3b83edef84d2e634770c1148d8cea1445d24e795
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-02 16:53:30 -0500

    Add nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import './styles/nav.css'
 import Main from './components/Main'
 import Instructions from './components/Instructions'
 import Ranking from './components/Ranking'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import Nav from './components/NavBar/Nav'
-
-export default function App () {
+import FormLogin from './components/FormLogin'
+export default function App() {
+  
   return (
     <>
-       <Nav />
-      <Routes>
-        <Route path="/" element={<Main />} />
+      <Nav />
+        <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="login" element={<FormLogin />} />
+        </Route>
         <Route path="/instructions" element={<Instructions />} />
         <Route path="/ranking" element={<Ranking />} />
       </Routes>
