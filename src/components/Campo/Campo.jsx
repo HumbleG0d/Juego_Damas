724fb6b3617b5e3f21bd64be9f4e84815bccf6e6
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-02 16:49:38 -0500

    Delete register

--- src/components/Campo/Campo.jsx
+++ src/components/Campo/Campo.jsx
@@ -1,21 +1,13 @@
-export default function Campo({ classNameLabel, classNameInput, htmlFor, name, type , register}) {
+import { validatorRules } from "../../js/validatorRules"
+
+export default function Campo({ classNameLabel, classNameInput, htmlFor, name, type, register }) {
     
-    const boolean = type === 'checkbox' ? false : true
+    const rules = validatorRules[type] 
+    
     return (
         <>
             <label className = {classNameLabel} htmlFor = {htmlFor} >{name}</label>
-            <input className={classNameInput} type={type} id={htmlFor} {...register(htmlFor, {
-                required: {
-                    value: boolean, message: `${name.toLowerCase()} es requerido`, ...(type === 'email' && { pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, message: 'correo invalido' } }), 
-                    ...(type === 'password' && {
-                        minLength: { value: 8, message: 'La contraseña debe tener al menos 8 caracteres' },
-                        pattern: {
-                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/,
-                            message: 'La contraseña debe contener al menos una letra mayúscula, un número, y un carácter especial'
-                        }
-                    })
-                }
-            })} />
+            <input className={classNameInput} type={type} id={htmlFor} {...register(htmlFor , rules)} />
         </>
     )
 }
