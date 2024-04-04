9fde91d9ddd25c70714471f0960a2c0c15d5d630
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-04 16:33:29 -0500

    Add route register

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Ranking from './components/Ranking'
 import { Routes, Route} from 'react-router-dom'
 import Nav from './components/NavBar/Nav'
 import FormLogin from './components/FormLogin'
+import FormCreatingAccount from './components/FormCreatingAccount'
 export default function App() {
   
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Routes>
         <Route path="/" element={<Main />}>
           <Route path="login" element={<FormLogin />} />
+          <Route path="register" element={<FormCreatingAccount />} />
         </Route>
         <Route path="/instructions" element={<Instructions />} />
         <Route path="/ranking" element={<Ranking />} />
