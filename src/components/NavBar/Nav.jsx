2cbcf9e8cda09213c2a2baf66484680feb89d8b9
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-05 17:05:47 -0500

    Add useLocation and Change nav

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,21 +1,38 @@
-import { Link } from "react-router-dom"
-export default function Nav() { 
-    return (
-        <header className="header">
-            <div className="div_header">
-                <a href="">
-                    <img src="/src/assets/logo/logo.png" alt="" className="img"/>
-                </a>
-            </div>
-            <nav className="navbar">
-            <ul className="links">
-                    <li className="link"><Link to="/">Home</Link></li>
-                    <li className="link"><Link to="/instructions">Instructions</Link></li>
-                    <li className="link"><Link to="/ranking">Ranking</Link></li>
-            </ul>
-        </nav>
+import { Link, useLocation } from "react-router-dom";
+export default function Nav() {
+  const { state } = useLocation();
+  return (
+    <header className="header">
+      <div className="div_header">
+        <Link to="/">
+          <img src="/src/assets/logo/logo.png" alt="" className="img" />
+        </Link>
+      </div>
 
-       </header>
-    
-    )
+      {state?.logged ? (
+        <div className="navbar_perfil">
+          <img src="" alt="" />
+          <label className="perfil">{state?.username}</label>
+          <select name="options perfil" id="">
+            <option value="nameperfil">View Profile</option>
+            <option value="salir">Sign Off </option>
+          </select>
+        </div>
+      ) : (
+        <nav className="navbar">
+          <ul className="links">
+            <li className="link">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="link">
+              <Link to="/instructions">Instructions</Link>
+            </li>
+            <li className="link">
+              <Link to="/ranking">Ranking</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
 }
