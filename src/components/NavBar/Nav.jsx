3f161be994af840cacfdf4e61b963af4c9547bc3
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-05 23:06:26 -0500

    Add button perfil and log aout

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,6 +1,18 @@
 import { Link, useLocation } from "react-router-dom";
+import Button from "../Button/Button";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { FaUserEdit } from "react-icons/fa";
+import { IoExitOutline } from "react-icons/io5";
+import { FaChartBar } from "react-icons/fa";
+import { useState } from "react";
 export default function Nav() {
   const { state } = useLocation();
+  const [visible, setVisible] = useState(false);
+
+  const onClick = () => {
+    setVisible(!visible);
+  };
+
   return (
     <header className="header">
       <div className="div_header">
@@ -10,13 +22,53 @@ export default function Nav() {
       </div>
 
       {state?.logged ? (
-        <div className="navbar_perfil">
-          <img src="" alt="" />
-          <label className="perfil">{state?.username}</label>
-          <select name="options perfil" id="">
-            <option value="nameperfil">View Profile</option>
-            <option value="salir">Sign Off </option>
-          </select>
+        <div className="paste_button">
+          <Button classNameButon="button_profile" onClick={onClick}>
+            <IoMdArrowDropdown
+              style={{
+                color: "#FFFFFF",
+                fontSize: "30px",
+              }}
+            />
+            <div className="button_username">{state?.username}</div>
+            <img
+              src="https://avatars.githubusercontent.com/u/95192731?v=4&s=48"
+              alt="img_profile"
+              className="img_profile"
+            />
+          </Button>
+
+          {visible && (
+            <ul className="dropdown">
+              <li className="dropdown_profile dropdown_link">
+                <FaUserEdit
+                  style={{
+                    color: "#5d5c5c",
+                    fontSize: "20px",
+                  }}
+                />
+                <Link to=" ">Your Profile</Link>
+              </li>
+              <li className="dropdown_statistics dropdown_link">
+                <FaChartBar
+                  style={{
+                    color: "#5d5c5c",
+                    fontSize: "20px",
+                  }}
+                />
+                <Link to="/ranking">statistics</Link>
+              </li>
+              <li className="dropdown_LogOut dropdown_link">
+                <IoExitOutline
+                  style={{
+                    color: "#5d5c5c",
+                    fontSize: "20px",
+                  }}
+                />
+                <Link to="/instructions">Log out</Link>
+              </li>
+            </ul>
+          )}
         </div>
       ) : (
         <nav className="navbar">
