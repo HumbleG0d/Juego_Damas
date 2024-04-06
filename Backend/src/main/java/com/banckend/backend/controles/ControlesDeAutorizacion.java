d3c7d6ab6baa8480fe62927ce14a39b4d97420eb
JhanCcanto <yoniccantoquispe@hotmail.com>
2024-04-06 13:59:56 -0500

    Se creo los controles de autorizacion para el registro y el acceso

--- Backend/src/main/java/com/banckend/backend/controles/ControlesDeAutorizacion.java
+++ Backend/src/main/java/com/banckend/backend/controles/ControlesDeAutorizacion.java
@@ -1,4 +1,38 @@
 package com.banckend.backend.controles;
 
+import com.banckend.backend.persistencia.entidades.EntidadUsuario;
+import com.banckend.backend.servicios.IAutorizacionServicios;
+import com.banckend.backend.servicios.modelos.datos.AccesoDatos;
+import com.banckend.backend.servicios.modelos.datos.RespuestaDatos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/auth")
 public class ControlesDeAutorizacion {
+
+    @Autowired
+    IAutorizacionServicios utorizacionServicios;
+
+    @PostMapping("/register")
+    private ResponseEntity<RespuestaDatos> register(@RequestBody EntidadUsuario usuario) throws Exception {
+        return new ResponseEntity<>(utorizacionServicios.registro(usuario), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    private ResponseEntity<HashMap<String,String>> acceso(@RequestBody AccesoDatos login) throws Exception {
+        HashMap<String,String> acceso = utorizacionServicios.acceso(login);
+        if (acceso.containsKey("jwt")){
+            return new ResponseEntity<>(acceso, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(acceso, HttpStatus.UNAUTHORIZED);
+        }
+    }
 }
