004e27026c501dddb06074eebeb677a446ba8c41
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-06 11:33:51 -0500

    Change of form design and addition of use navigate

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,75 +1,105 @@
-import Form from "./Form/Form"
-import Campo from "./Campo/Campo"
-import Button from "./Button/Button"
-import "../styles/normalize.css"
-import "../styles/form.css"
-import { useForm } from "react-hook-form"
+import Form from "./Form/Form";
+import Campo from "./Campo/Campo";
+import Button from "./Button/Button";
+import "../styles/normalize.css";
+import "../styles/form.css";
+import { useForm } from "react-hook-form";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link , Outlet , useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 
-export default function FormRegister () {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+export default function FormRegister() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    watch
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+      passwordConfirm: "",
+      correo: "",
+    },
+  });
 
   const navigate = useNavigate();
 
   const onSubmit = handleSubmit((data) => {
-    console.log(data)
-    navigate('/play')
-  })
+    navigate("/login", {
+      replace: true,
+      state: {
+        logged1: watch("password") === watch("passwordConfirm") ? true : false,
+      },
+    });
+    reset();
+  });
 
   return (
     <>
-    <div className="div_form form-container">
-      <Link to="/">
-        <AiFillCloseCircle className="close" />
-       
+      <div className="div_form form-container">
+        <Link to="/">
+          <AiFillCloseCircle className="close" />
         </Link>
         <p className="title">Register</p>
-        
-      <Form onSubmit={onSubmit} className="form">
-        <div className="input-group">
-          <Campo
-            classNameLabel="form_label"
-            classNameInput="form_input"
-            htmlFor="username"
-            name="Username"
+
+        <Form onSubmit={onSubmit} className="form">
+          <div className="input-group">
+            <Campo
+              classNameLabel="form_label"
+              classNameInput="form_input"
+              htmlFor="username"
+              name="Username"
               type="text"
               register={register}
             />
-            {errors.username && <span className="form_error">{errors.username.message}</span>}
+            {errors.username && (
+              <span className="form_error">{errors.username.message}</span>
+            )}
+          </div>
+          <div className="input-group">
+            <Campo
+              classNameLabel="form_label"
+              classNameInput="form_input"
+              htmlFor="password"
+              name="Password"
+              type="password"
+              register={register}
+            />
+            {errors.password && (
+              <span className="form_error">{errors.password.message}</span>
+            )}
+          </div>
 
-        </div>
-        <div className="input-group">
-          <Campo
-            classNameLabel="form_label"
-            classNameInput="form_input"
-            htmlFor="password"
-            name="Password"
+          <div className="input-group">
+            <Campo
+              classNameLabel="form_label"
+              classNameInput="form_input"
+              htmlFor="passwordConfirm"
+              name="Confirm Password"
               type="password"
               register={register}
             />
-            {errors.password && <span className="form_error">{errors.password.message}</span>}
+            {errors.password && (
+              <span className="form_error">{errors.password.message}</span>
+            )}
           </div>
-          
           <div className="input-group">
-          <Campo
-            classNameLabel="form_label"
-            classNameInput="form_input"
-            htmlFor="password1"
-            name="Confirm Password"
-              type="password"
+            <Campo
+              classNameInput="form_input"
+              classNameLabel="form_label-id"
+              htmlFor="correo"
+              name="Email"
+              type="email"
               register={register}
             />
-            {errors.password && <span className="form_error">{errors.password.message}</span>}
+            {errors.correo && (
+              <span className="form_error">{errors.correo.message}</span>
+            )}
           </div>
-          <div className="input-group">
-                <Campo classNameInput="form_input" classNameLabel="form_label-id" htmlFor="correo" name="Email" type="email" register={register} />
-                {errors.correo && <span className="form_error">{errors.correo.message}</span>}
-            </div>
-        <Button classNameButon="register">Register</Button>
-      </Form>
-    </div>
-    <Outlet/>
+          <Button classNameButon="register">Register</Button>
+        </Form>
+      </div>
     </>
-  )
+  );
 }
