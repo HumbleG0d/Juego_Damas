1d89a446e969b52699e4265b05ab9a527585f29c
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-08 00:08:29 -0500

    Add the logic for the placement of the pieces

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,35 @@
 import "../styles/board.css";
 import crearTablero from "../js/matrizBoard";
 import Square from "./Square/Square";
+import Piece from "./Piece/Piece";
+import '../styles/piece.css';
+export default function Board() {
 
-export default function Board() { 
-    const tablero = crearTablero();
+    const renderizarPiezas = (indexFila , indexCasilla) => {
+        if(indexFila < 3 && (indexFila + indexCasilla) % 2 == 0){
+            return <Piece color="#2c0e0d"/>
+        }
+        if(indexFila > 4 && (indexFila + indexCasilla) % 2 !== 0){
+            return <Piece color="#dca658"/>
+        }
+    }
+
+  const tablero = crearTablero();
   return (
     <section className="tablero">
-      {tablero.map((fila, indexFila) => (
+    {tablero.map((fila, indexFila) => (
         <div key={indexFila} className="casillero_filas">
-          {fila.map((casilla, indexCasilla) => (
-            <Square key={indexCasilla} color={(indexFila + indexCasilla) % 2 === 0 ? "#FAEDDB" : "#BB613D" } />
-          ))}
+            {fila.map(
+                (casilla, indexCasilla) => (
+                    <Square key={indexCasilla} className="casillero_columnas"
+                    color={(indexFila + indexCasilla) % 2 === 0 ? "#FAEDDB" : "#BB613D"}
+                    >
+                        {renderizarPiezas(indexFila, indexCasilla)}
+                    </Square>
+                )
+            )}
         </div>
-      ))}
+    ))}
     </section>
   );
 }
