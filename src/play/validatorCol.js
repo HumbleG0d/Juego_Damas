64da26a624867275113e4ca347f9fa231355f316
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-19 15:53:03 -0500

    Add logic to return the column value

---
+++ src/play/validatorCol.js
@@ -0,0 +1,22 @@
+export default function validatorCol(value) {
+  switch (value.toLowerCase()) {
+    case "a":
+      return 0;
+    case "b":
+      return 1;
+    case "c":
+      return 2;
+    case "d":
+      return 3;
+    case "e":
+      return 4;
+    case "f":
+      return 5;
+    case "g":
+      return 6;
+    case "h":
+      return 7;
+    default:
+      return null;
+  }
+}
